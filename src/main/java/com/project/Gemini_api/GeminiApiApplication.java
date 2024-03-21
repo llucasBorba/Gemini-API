4eb70a157690fa558cbc9be49533989ea0fafb22
llucasBorba <lucasborba6075@gmail.com>
2024-03-20 21:43:18 -0300

    changes

---
+++ src/main/java/com/project/Gemini_api/GeminiApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.Gemini_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiApiApplication.class, args);
+	}
+
+}
